3ca86ae8c366ff4d80d56b33ea61c1d202914687
Rawi Lahiany <rawipro.rl@gmail.com>
2021-10-27 11:06:20 +0300

    header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Header from './header/Header'
 import { BrowserRouter, Route } from 'react-router-dom'
-
+import "./app.css"
 export default function App() {
     return (
         <div>
--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -1,24 +1,39 @@
-import React,{useState,useEffect} from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import './header.css';
 
 export default function Header() {
-    const [logedIn,setLogedIn]=useState(true)
+	const [ logedIn, setLogedIn ] = useState(true);
 
-    return (
-        <div>
-            <div className="header">
-                <nav>
-                    <ul>
-                        <li ><Link to="/" className="logo"></Link></li>
-                        <li><Link to="/account">Account</Link></li>
-                        <li><Link to="/transition">Transition</Link></li>
-                    </ul>
-                    <div>
-                        {logedIn?<Link onClick={()=>{setLogedIn(false)}} to="/">Log Out</Link>:
-                        <Link to="/">LogIn</Link>}
-                    </div>
-                </nav>
-            </div>
-        </div>
-    )
+	return (
+		<div className="header">
+			<nav>
+				<ul>
+						<div className="logo">
+							<Link to="/" />
+						</div>
+					<li>
+						<Link to="/account">Account</Link>
+					</li>
+					<li>
+						<Link to="/transition">Transition</Link>
+					</li>
+				</ul>
+				<div className="login">
+					{logedIn ? (
+						<Link
+							onClick={() => {
+								setLogedIn(false);
+							}}
+							to="/"
+						>
+							Log Out
+						</Link>
+					) : (
+						<Link to="/">LogIn</Link>
+					)}
+				</div>
+			</nav>
+		</div>
+	);
 }
